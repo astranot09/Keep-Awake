26c1789c5e196716a73ed4a6a0c1ba4656db6eac
astranot09 <anthony.setiawan002@binus.ac.id>
2026-05-10 11:57:40 +0700

    s

--- Keep Awake/Assets/Script/Lecture/LecturerScript.cs
+++ Keep Awake/Assets/Script/Lecture/LecturerScript.cs
@@ -3,30 +3,53 @@ using UnityEngine;
 
 public class LecturerScript : MonoBehaviour
 {
-    [SerializeField] private float minDelay;
-    [SerializeField] private float maxDelay;
-    [SerializeField] private float delay;
+    [Header("Teaching")]
+    [SerializeField] private float minTeachingDelay = 3f;
+    [SerializeField] private float maxTeachingDelay = 7f;
+
+    [Header("Looking")]
+    [SerializeField] private float minLookingDelay = 2f;
+    [SerializeField] private float maxLookingDelay = 4f;
+
+    [Header("State")]
     [SerializeField] private bool isTeaching;
-
-
-    [SerializeField] private float lookingTime;
     [SerializeField] private bool isLooking;
 
-    private void Update()
-    {
+    private SpriteRenderer spriteRenderer;
 
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(TeachingState());
     }
 
-    public void RandomDelay()
-    {
-        delay = Random.Range(minDelay, maxDelay);
-    }
-
-    private IEnumerator LectureTeaching(float delay)
+    private IEnumerator TeachingState()
     {
         isTeaching = true;
+        isLooking = false;
+
+        float delay = Random.Range(minTeachingDelay, maxTeachingDelay);
+
+        Debug.Log("Guru mengajar");
+        spriteRenderer.color = Color.white;
+
         yield return new WaitForSeconds(delay);
+
+        StartCoroutine(LookingState());
+    }
+
+    private IEnumerator LookingState()
+    {
         isTeaching = false;
-        //nengok
+        isLooking = true;
+
+        float delay = Random.Range(minLookingDelay, maxLookingDelay);
+
+        Debug.Log("Guru nengok");
+        spriteRenderer.color = Color.red;
+
+        yield return new WaitForSeconds(delay);
+
+        StartCoroutine(TeachingState());
     }
 }
---
+++ Keep Awake/Assets/Script/Phone/PhoneScript.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class PhoneScript : MonoBehaviour
+{
+    [Header("Panels")]
+    [SerializeField] private GameObject homePagePanel;
+    [SerializeField] private GameObject scrollingPanel;
+    [SerializeField] private GameObject internetPanel;
+    [SerializeField] private GameObject settingPanel;
+    [SerializeField] private GameObject mainMenuPanel;
+
+    [Header("Phone")]
+    [SerializeField] private GameObject phonePanel;
+
+    private void Start()
+    {
+        OpenPanel(homePagePanel);
+    }
+
+    public void OpenScrolling()
+    {
+        OpenPanel(scrollingPanel);
+    }
+
+    public void OpenInternet()
+    {
+        OpenPanel(internetPanel);
+    }
+
+    public void OpenSetting()
+    {
+        OpenPanel(settingPanel);
+    }
+
+    public void OpenMainMenu()
+    {
+        OpenPanel(mainMenuPanel);
+    }
+
+    public void OpenHomePage()
+    {
+        OpenPanel(homePagePanel);
+    }
+    public void Phone()
+    {
+        phonePanel.SetActive(!phonePanel.activeSelf);
+    }
+
+
+    private void OpenPanel(GameObject targetPanel)
+    {
+        // matiin semua panel
+        homePagePanel.SetActive(false);
+        scrollingPanel.SetActive(false);
+        internetPanel.SetActive(false);
+        settingPanel.SetActive(false);
+        mainMenuPanel.SetActive(false);
+
+        // nyalain target
+        targetPanel.SetActive(true);
+    }
+}
